7c90c91d13961fc3dd2c7eb7e914bbc393ca96ce
TanithFlory <tanithflory7770@gmail.com>
2024-08-02 18:35:05 +0530

    Update table, feature/price-comparison: postpone

--- app/components/PriceComparison/PriceComparison.tsx
+++ app/components/PriceComparison/PriceComparison.tsx
@@ -5,7 +5,7 @@ import DashboardCardTitle from "@/app/utils/Dashboard/DashboardCardTitle";
 export default function PriceComparison() {
   const otherStores = ["Flipkart", "Croma", "Ajio", "Myntra"];
   return (
-    <DashboardCard>
+    <DashboardCard className="relative">
       <DashboardCardTitle
         title="Price Comparison"
         description="Compare the prices from other websites."
@@ -14,6 +14,9 @@ export default function PriceComparison() {
         {otherStores.map((item, index) => (
           <PrinceComparisonCard item={item} key={index} />
         ))}
+      </div>
+      <div className="absolute inset-0 flex items-center justify-center bg-[rgba(0,0,0,0.7)]  text-white text-lg font-bold">
+        <span>Coming Soon...</span>
       </div>
     </DashboardCard>
   );
